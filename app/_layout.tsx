724d7f57e7a7ae92733427edf45063ad3c92725d
Simiel <samensah036@st.ug.edu.gh>
2024-03-05 19:30:18 +0000

    managed notifications success

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,188 +1,97 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useRef, useState } from 'react';
+import { Button, Platform, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
-import { Button, Platform, StyleSheet, TouchableOpacity } from 'react-native';
-import ModalScreen from './modal';
-import { Text, View } from '@/components/Themed';
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { StatusBar } from 'expo-status-bar';
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-// notifications
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: true,
-    shouldSetBadge: true,
+    shouldSetBadge: false,
   }),
 });
 
-// register for push notifications
-async function registerForPushNotificationsAsync() {
-  let token;
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = await Notifications.getExpoPushTokenAsync({
-      projectId: '936263a2-7c6f-461b-bb92-f602018ac994',
-    });
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  return token?.data;
-}
-
-async function sendPushNotification(expoPushToken: string) {
-  console.log('sending push notification');
-  const message = {
-    to: expoPushToken,
-    sound: 'default',
-    title: 'Simiel Title',
-    body: 'Vamosss!',
-    data: { someData: 'goes here' },
-  };
-
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    headers: {
-      host: 'exp.host',
-      Accept: 'application/json',
-      'Accept-encoding': 'gzip, deflate',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(message),
-  });
-
-  console.log('push notification sent');
-}
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
+export default function App() {
   const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef<Notifications.Subscription>();
-  const responseListener = useRef();
 
   useEffect(() => {
-    registerForPushNotificationsAsync().then((token) =>
-      setExpoPushToken(token || '')
-    );
-
-    console.log('Token:', expoPushToken);
-
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-      });
-
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
-
-    return () => {
-      Notifications.removeNotificationSubscription(
-        notificationListener.current
-      );
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
+    console.log('Registering for push notifications...');
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        console.log('token: ', token);
+        setExpoPushToken(token);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+  async function registerForPushNotificationsAsync() {
+    let token;
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      // Learn more about projectId:
+      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId: '936263a2-7c6f-461b-bb92-f602018ac994',
+        })
+      ).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+
+    return token;
   }
 
+  const sendNotification = async () => {
+    console.log('Sending push notification...');
+
+    // notification message
+    const message = {
+      to: expoPushToken,
+      sound: 'default',
+      title: 'My first push notification!',
+      body: 'This is my first push notification made with expo rn app',
+    };
+
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        host: 'exp.host',
+        accept: 'application/json',
+        'accept-encoding': 'gzip, deflate',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{expoPushToken}</Text>
-      <View
-        style={styles.separator}
-        lightColor='#eee'
-        darkColor='rgba(255,255,255,0.1)'
-      />
-      {/* <EditScreenInfo path='app/modal.tsx' /> */}
-
-      {/* button */}
-      <TouchableOpacity
-        onPress={() => {
-          sendPushNotification(expoPushToken);
-        }}
-      >
-        <Text>bgjhguv</Text>
-      </TouchableOpacity>
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    <View style={{ marginTop: 100, alignItems: 'center' }}>
+      <Text style={{ marginVertical: 30 }}>
+        Expo RN Push Notifications: {expoPushToken}
+      </Text>
+      <Button title='Send push notification' onPress={sendNotification} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
